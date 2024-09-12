6b37eecc428e1a418ff309be4fbff851dc60073c
Jacob Knowlton <knowlton.jacob17@gmail.com>
2024-09-12 14:27:50 -0600

    Added scaffolding for optimizations

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,9 @@
+mod parser;
+use parser::parse;
+use parser::Command;
+
 const INIT_TAPE_SIZE: usize = 65536;
 const INIT_POINTER_LOC: usize = INIT_TAPE_SIZE / 2;
-
-#[derive(Debug)]
-enum Command {
-    IncPointer,
-    DecPointer,
-    IncData,
-    DecData,
-    Output,
-    Input,
-    JumpForward { idx: usize },
-    JumpBack { idx: usize },
-}
-
-fn parse(src: String) -> Vec<Command> {
-    let mut commands: Vec<Command> = vec![];
-    let mut stack: Vec<usize> = vec![];
-    let mut idx = 0;
-    for c in src.chars() {
-        let op = match c {
-            '>' => Some(Command::IncPointer),
-            '<' => Some(Command::DecPointer),
-            '+' => Some(Command::IncData),
-            '-' => Some(Command::DecData),
-            '.' => Some(Command::Output),
-            ',' => Some(Command::Input),
-            '[' => {
-                stack.push(idx);
-                // idx will be changed when the matching ']' is encountered
-                Some(Command::JumpForward { idx: 0 })
-            },
-            ']' => {
-                let prev_idx = match stack.pop() {
-                    Some(value) => value,
-                    None => {
-                        eprintln!("Error parsing input: Unmatched ']'");
-                        std::process::exit(1);
-                    }
-                };
-                // The command at prev_idx should always be JumpForward
-                if let Command::JumpForward { idx: forward_idx } = &mut commands[prev_idx] {
-                    *forward_idx = idx;
-                } else {
-                    eprintln!("Error parsing input: Unexpected character");
-                    std::process::exit(1);
-                }
-                Some(Command::JumpBack { idx: prev_idx })
-            },
-            _ => None,
-        };
-
-        if let Some(op) = op {
-            commands.push(op);
-            idx += 1;
-        }
-    }
-
-    if stack.len() > 0 {
-        eprintln!("Error parsing input: Unmatched '['");
-        std::process::exit(1);
-    }
-
-    commands
-}
 
 fn interp(commands: Vec<Command>) {
     let mut tape: Vec<u8> = vec![0; INIT_TAPE_SIZE];
@@ -72,10 +13,11 @@ fn interp(commands: Vec<Command>) {
 
     while pc < commands.len() {
         match commands[pc] {
-            Command::IncPointer => pointer += 1,
-            Command::DecPointer => pointer -= 1,
-            Command::IncData => tape[pointer] = tape[pointer].wrapping_add(1),
-            Command::DecData => tape[pointer] = tape[pointer].wrapping_sub(1),
+            Command::IncPointer { amount } => pointer += amount,
+            Command::DecPointer { amount } => pointer -= amount,
+            Command::IncData { offset, amount } => tape[pointer.saturating_add_signed(offset)] = tape[pointer].wrapping_add(amount),
+            Command::DecData { offset, amount } => tape[pointer.saturating_add_signed(offset)] = tape[pointer].wrapping_sub(amount),
+            Command::ZeroData { offset } => tape[pointer.saturating_add_signed(offset)] = 0,
             Command::Output => {
                 match char::from_u32(u32::from(tape[pointer])) {
                     Some(c) => print!("{}", c),
