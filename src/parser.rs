f0d205c63388d035358687ae1fe09e3efc00bf9a
Jacob Knowlton <knowlton.jacob17@gmail.com>
2024-09-17 16:27:58 -0600

    Sorted profile loops by executions

--- src/parser.rs
+++ src/parser.rs
@@ -87,7 +87,12 @@ pub fn is_simple_loop(loop_cmd: &Command) -> bool {
 
 
 pub fn print_profile(commands: &[Command]) {
-    fn print_profile_rec(commands: &[Command], curr_idx: &mut usize, simple_loops: &mut Vec<usize>, non_simple_loops: &mut Vec<usize>) {
+    struct LoopData {
+        idx: usize,
+        num_executions: usize,
+    }
+
+    fn print_profile_rec(commands: &[Command], curr_idx: &mut usize, simple_loops: &mut Vec<LoopData>, non_simple_loops: &mut Vec<LoopData>) {
         for command in commands {
             match command {
                 Command::IncPointer { count } => { 
@@ -110,9 +115,9 @@ pub fn print_profile(commands: &[Command]) {
                 },
                 Command::Loop { body, start_count, end_count } => {
                     if is_simple_loop(command) {
-                        simple_loops.push(*curr_idx);
+                        simple_loops.push(LoopData { idx: *curr_idx, num_executions: *end_count });
                     } else {
-                        non_simple_loops.push(*curr_idx);
+                        non_simple_loops.push(LoopData { idx: *curr_idx, num_executions: *end_count });
                     }
 
                     println!("{:>8} : [ : {}", curr_idx, start_count);
@@ -130,15 +135,18 @@ pub fn print_profile(commands: &[Command]) {
 
     // Driver for recursive method
     let mut init_index = 0;
-    let mut simple_loops: Vec<usize> = vec![];
-    let mut non_simple_loops: Vec<usize> = vec![];
+    let mut simple_loops: Vec<LoopData> = vec![];
+    let mut non_simple_loops: Vec<LoopData> = vec![];
     print_profile_rec(&commands, &mut init_index, &mut simple_loops, &mut non_simple_loops);
 
+    simple_loops.sort_by(|a, b| b.num_executions.cmp(&a.num_executions));
+    non_simple_loops.sort_by(|a, b| b.num_executions.cmp(&a.num_executions));
+
     for simple_loop in simple_loops {
-        println!("Simple loop at index {}", simple_loop);
+        println!("Simple loop at index {}, executions: {}", simple_loop.idx, simple_loop.num_executions);
     }
     for non_simple_loop in non_simple_loops {
-        println!("Non-simple loop at index {}", non_simple_loop);
+        println!("Non-simple loop at index {}, executions: {}", non_simple_loop.idx, non_simple_loop.num_executions);
     }
 }
 
